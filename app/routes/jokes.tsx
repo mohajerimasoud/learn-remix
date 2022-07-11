0e62d742aa5c771a166aff45d682cfb9eaf4c925
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-07-11 17:53:06 +0430

    get one

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,5 +1,4 @@
 import type { LinksFunction, LoaderFunction } from "@remix-run/node";
-import type { Joke } from "@prisma/client";
 import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
