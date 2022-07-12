22b588e2cd6ffc4cac765b824aeb09b5d1695bea
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-07-12 16:22:36 +0430

    seo and add meta tags

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,32 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { useLoaderData, Link } from "@remix-run/react";
-import type { Joke } from "@prisma/client";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
-import { db } from "~/utils/db.server";
+import stylesUrl from "~/styles/index.css";
 
-type LoaderData = { randomJoke: Joke };
-
-export const loader: LoaderFunction = async () => {
-  const count = await db.joke.count();
-  const randomRowNumber = Math.floor(Math.random() * count);
-  const [randomJoke] = await db.joke.findMany({
-    take: 1,
-    skip: randomRowNumber,
-  });
-  const data: LoaderData = { randomJoke };
-  return json(data);
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default function JokesIndexRoute() {
-  const data = useLoaderData<LoaderData>();
+export const meta: MetaFunction = () => ({
+  title: "Remix: So great, it's funny!",
+  description: "Remix jokes app. Learn Remix and laugh at the same time!",
+});
 
+export default function IndexRoute() {
   return (
-    <div>
-      <p>Here's a random joke:</p>
-      <p>{data.randomJoke.content}</p>
-      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
